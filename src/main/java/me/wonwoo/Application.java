9d691559a2dce4b016a406f8e1c57c66e05e72cd
wonwoo <aoruqjfu@gmail.com>
2016-05-06 14:15:12 +0900

    init url short

--- src/main/java/me/wonwoo/Application.java
+++ src/main/java/me/wonwoo/Application.java
@@ -1,12 +1,31 @@
 package me.wonwoo;
 
+import me.wonwoo.shrotener.domain.Sequence;
+import me.wonwoo.shrotener.repository.SequenceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Autowired
+	private SequenceRepository sequenceRepository;
+
+	@Bean
+	public CommandLineRunner commandLineRunner(){
+		return args -> {
+			Sequence sequence = sequenceRepository.findOne("urlShort");
+			if (sequence == null){
+				sequence = new Sequence("urlShort", 1L);
+				sequenceRepository.save(sequence);
+			}
+		};
 	}
 }
